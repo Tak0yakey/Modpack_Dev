6ccdc83724d27fcf5bd2d3fd9f789bfdb49630dd
thunderalpaca <156068084+thunderalpaca@users.noreply.github.com>
2026-04-23 18:14:24 +0900

    なえがねえ

--- pack/overrides/kubejs/server_scripts/market.js
+++ pack/overrides/kubejs/server_scripts/market.js
@@ -1,6 +1,5 @@
 ServerEvents.recipes(event => {
-    const spawn_eggs = Ingredient.of('%minecraft:spawn_eggs').itemIds
-    spawn_eggs.forEach(spawn_egg => {
+    Ingredient.of('%minecraft:spawn_eggs').itemIds.forEach(spawn_egg => {
         if(spawn_egg != 'minecraft:spawner' && spawn_egg != 'minecraft:trial_spawner'){
             event.custom({
                 "type": "farmingforblockheads:market",
@@ -13,7 +12,19 @@ ServerEvents.recipes(event => {
             })
         }
     })
-
+    Ingredient.of('#supplementaries:flower_box_plantable').itemIds.forEach(plant => {
+        if(!Ingredient.of(/minecraft.*sapling/).test(plant) && plant != 'supplementaries:flax_seeds' && plant != 'culturaldelights:avocado_sapling'){
+            event.custom({
+                "type": "farmingforblockheads:market",
+                "category": "farmingforblockheads:saplings",
+                "preset": "minecraft:saplings",
+                "result": {
+                    "count": 1,
+                    "item": plant
+                }
+            })
+        }
+    })
     event.remove({ id: 'farmingforblockheads:market' })
     event.shaped(
         Item.of('farmingforblockheads:market',1),
